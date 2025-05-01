232330f44b6bb04f62c03de38ae561e74c924b1a
Tadeo Aguilar <tadeo.am@gmail.com>
2025-05-01 17:46:55 -0400

    API and Headers

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,41 @@
-import Image from "next/image";
+//import Image from "next/image";
 
-export default function Home() {
+
+
+export default async function Home() {
+  const data = await fetch("https://diprec.acumatica.com/entity/Default/23.200.001//Bill?$top=10&$filter=Vendor eq 'MAP0036'&$select=Amount,Vendor,ReferenceNbr",
+    {
+      headers: {
+        "Accept": "application/json",
+        "Authorization": `Bearer D7oT1pBt1XwcaGac6-geDsjEtWsAvbJrDcGcw6RDKek`,
+        "Content-Type": 'application/json',
+        "Cookie": ".ASPXAUTH=067EC596C12B2642BBC1E16E2E1DA76E763820009856B4127213E247DD544382BEB1095101D06F5DDDDE1794DE2206DE7DB636A49D635685F6BEFDAE3FEC47496D9587192F7E88A9CE8419E0EA15903A80DF680D18499A5E6042D3CFB57B3EB507F6FB27DB5CB2EBAB8846D6D6FEC5E13C2E979C1F9276814C2AE7E52071F7A7D647898AF99343A49358E0ABB18869DDFE36516D; ASP.NET_SessionId=j3ixra2vwzkvaiuogai3i2vq; CompanyID=ZUBRAUN; Locale=TimeZone=GMTM0600M&Culture=en-US; UserBranch=1; requestid=4FBDD1C36B026D8311F026D0EDF9472D; requeststat=+st:586+sc:~/entity/auth/login+start:638817307322935720+tg:"
+    }
+  }
+    
+  )
+  const posts = await data.json()
+  //console.log(posts)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      
+      {
+      posts.map((post: {id:string, ReferenceNbr:{value: number},  Vendor:{value: number}} ) => (
+        
+       console.log(post)
+        
+      ))
+      }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+      {posts.map((post: {id:string, ReferenceNbr:{value: number},  Vendor:{value: number}} ) => (
+        
+        <li key={post.id}> {post.ReferenceNbr.value} - {post.Vendor.value}</li>
+        
+      ))}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+       
       </footer>
     </div>
   );
